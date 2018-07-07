33d4c11e9c0ce21b3b7063b09e01c90c6ceaf2d7
Rick <wfaengrit@gmail.com>
2018-07-07 21:01:53 +0700

    change code

--- hello.py
+++ hello.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 
-def source():
-    html = 'Hello World!'
-    return html
+def hello():
+    return "Hello"
+
+if __name__=="__main__":
+    app.run()
 
