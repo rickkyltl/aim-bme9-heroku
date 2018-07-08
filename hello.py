0e3526fc785edd1287187abeb53893e0057e0309
Rick <wfaengrit@gmail.com>
2018-07-08 12:23:49 +0700

    test api

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
-from flask import Flask
-
+from flask import Flask, request
+import requests
+text = "hello world"
 app = Flask(__name__)
+@app.route('/')
+def index(text):
+  return text
 
-@app.route("/")
-
-def hello():
-    return "Hello"
+# ส่วน callback สำหรับ Webhook
+@app.route('/callback', methods=['POST'])
+def callback():
+  message = request.get()
+  index(message+"finish") 
+  return '',200
 
 if __name__=="__main__":
     app.run()
